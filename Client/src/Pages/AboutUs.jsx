2c66cba3d18625f00cda79e86a2e426eaa5bc8aa
sakshamshukla02 <sshuklaknp2002@gmail.com>
2025-03-18 13:41:55 +0530

    added about us and contact us

--- Client/src/Pages/AboutUs.jsx
+++ Client/src/Pages/AboutUs.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import Navbar from "../Components/Navbar"
+import Footer from "../Components/Footer"
 const AboutUs = () => {
   return (
-    <div>
-      Hello AboutUs
+    <>
+    <Navbar/>
+    <div className='mt-45 ml-20 flex mb-2'>
+     <div className="flex w-1/2 " >
+        <img src="https://jrssnacks.com/wp-content/uploads/2024/10/WhatsApp-Image-2024-10-24-at-13.18.56_dd82a117.jpg" alt="" className='h-120'/>
+     </div>
+     <div className="flex-col w-1/2 ">
+      <p className='m-4 text-2xl font-semibold'>JRS Snacks</p>
+      <p className='m-4 text-3xl font-bold'>About our online store</p>
+      <p className='m-4 text-2xl font-semibold text-gray-500'>About Us:</p>
+      <p className='m-4 text-2xl text-gray-400'>JRS is a frozen foods manufacturer that not only believes in great taste, but also in a variety of great palates. What began as a humble hustle during the 90s, our products have grown over the years to create an established and well-trusted brand of their own. Each of our snacks is a reflection of our consistent drive to innovate and create nothing less than the best.</p>
+     </div>
     </div>
+    <Footer/>
+    </>
   );
 }
 
--- Client/src/Pages/ContactUs.jsx
+++ Client/src/Pages/ContactUs.jsx
@@ -1,11 +1,61 @@
-import React from 'react';
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 const ContactUs = () => {
   return (
-    <div>
-      Hello ContactUs
-    </div>
+    <>
+      <Navbar />
+      <div className="mt-50">
+        <div></div>
+        <div className="flex mt-10 mb-10 ml-10 mr-10 ">
+          <div className="w-1/2 border-r-2 border-gray-200 p-2">
+            <p className="font-bold" >JRS Snacks</p>
+            <p className="font-bold"> Contact Us</p>
+            <hr className="text-gray-200" />
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem
+              dolor quibusdam odit temporibus consequatur perferendis error eius
+              ullam quasi quaerat!
+            </p>
+            <ul>
+              <li>Address : Lorem ipsum dolor sit amet.</li>
+              <li>Email : Lorem, ipsum.</li>
+              <li>Phone : Lorem, ipsum.</li>
+            </ul>
+          </div>
+          <div className="ml-4 w-1/2">
+            <p className="font-bold"> INFORMATION ABOUT US</p>
+            <p className="font-bold">CONTACT US FOR ANY QUESTIONS</p>
+            <form action="" className="mt-8 flex flex-wrap gap-x-50 gap-y-10 mb-5">
+              <div>
+                <label>Your Name</label>
+                <input type="text" name="" id="" className="border-2 block" />
+              </div>
+              <div>
+                <label>Your Email</label>
+                <input type="text" name="" id="" className="border-2 block" />
+              </div>
+              <div>
+                <label>Phone Number</label>
+                <input type="text" name="" id="" className="border-2 block" />
+              </div>
+              <div>
+                <label>Company</label>
+                <input type="text" name="" id="" className="border-2 block" />
+              </div>
+              <div>
+                <label>Your Message</label>
+                <textarea class=" w-150 h-40 border p-2 block"></textarea>
+              </div>
+            </form>
+            <p>Ask A Question</p>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
-}
+};
 
 export default ContactUs;
