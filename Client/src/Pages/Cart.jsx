9d68f93a7f68503272410dc542c94d3f6e472655
Ashish Singh <ashuisashish333@gmail.com>
2025-03-21 17:01:17 +0530

    payment integration successfully done

--- Client/src/Pages/Cart.jsx
+++ Client/src/Pages/Cart.jsx
@@ -9,11 +9,10 @@ import {
   removeOrder,
   decrementOrder,
 } from "../Reducer/Order/orderSlice";
+import axios from "axios";
 
 const Cart = () => {
   const orders = useSelector((state) => state.orderReducer.orders);
-  // console.log(orders);
-  
   const dispatch = useDispatch();
 
   return (
@@ -32,27 +31,23 @@ const Cart = () => {
           <h1 className="text-xl text-white font-bold">ORDER COMPLETE</h1>
         </div>
       </div>
-<div className="container mx-auto mt-10 flex">
-      {orders && orders.length>0 ?(
-        <>
-          <Full orders={orders} dispatch={dispatch} />
-          <CartTotals orders={orders} />
-        </>
-      ):(
-        <Empty/>
-      )}
-        
+      <div className="container mx-auto mt-10 flex">
+        {orders && orders.length > 0 ? (
+          <>
+            <Full orders={orders} dispatch={dispatch} />
+            <CartTotals orders={orders} />
+          </>
+        ) : (
+          <Empty />
+        )}
       </div>
     </div>
   );
 };
 
 const Full = ({ orders, dispatch }) => {
-  const handleRemove = (id) => {
-    dispatch(removeOrder({ id }));
-  };
-
-  const handleIncrement = (item) => {
+  const handleRemove = (id) => dispatch(removeOrder({ id }));
+  const handleIncrement = (item) =>
     dispatch(
       addOrder({
         id: item.id,
@@ -62,11 +57,7 @@ const Full = ({ orders, dispatch }) => {
         quantity: 1,
       })
     );
-  };
-
-  const handleDecrement = (id) => {
-    dispatch(decrementOrder({ id }));
-  }
+  const handleDecrement = (id) => dispatch(decrementOrder({ id }));
 
   return (
     <div className="w-3/4">
@@ -78,114 +69,135 @@ const Full = ({ orders, dispatch }) => {
             <th className="py-2 px-4 text-left">Product</th>
             <th className="py-2 px-4 text-left">Price</th>
             <th className="py-2 px-4 text-left">Quantity</th>
-            <th className="py-2 px-4 text-left">Subtotal</th>
+            <th className="py-2 px-4 text-left">Total</th>
           </tr>
         </thead>
         <tbody>
-          {orders && orders.length > 0 ? (
-            orders.map((item) => (
-              <tr key={item.id} className="border-b border-gray-200">
-                <td className="py-4 px-4 text-center">
+          {orders.map((item) => (
+            <tr key={item.id} className="border-b border-gray-200">
+              <td className="py-4 px-4 text-center">
+                <button
+                  className="text-red-600 hover:text-red-800 cursor-pointer"
+                  onClick={() => handleRemove(item.id)}
+                >
+                  ×
+                </button>
+              </td>
+              <td className="py-4 px-4">
+                <img
+                  src={item.imageUrl}
+                  alt={item.name}
+                  className="w-16 h-16 object-cover"
+                />
+              </td>
+              <td className="py-4 px-4">{item.name}</td>
+              <td className="py-4 px-4">₹{item.cost.toFixed(2)}</td>
+              <td className="py-4 px-4">
+                <div className="flex items-center">
                   <button
-                    className="text-red-600 hover:text-red-800 cursor-pointer"
-                    onClick={() => handleRemove(item.id)}
+                    className="px-2 py-1 border-2 border-gray-300 text-gray-600 cursor-pointer hover:bg-emerald-900 hover:text-white"
+                    onClick={() => handleDecrement(item.id)}
                   >
-                    ×
+                    -
                   </button>
-                </td>
-                <td className="py-4 px-4">
-                  <img
-                    src={item.imageUrl}
-                    alt={item.name}
-                    className="w-16 h-16 object-cover"
+                  <input
+                    type="text"
+                    value={item.quantity}
+                    className="w-12 px-2 py-1 text-center border-t-2 border-b-2 border-gray-300"
+                    readOnly
                   />
-                </td>
-                <td className="py-4 px-4">{item.name}</td>
-                <td className="py-4 px-4">₹{item.cost.toFixed(2)}</td>
-                <td className="py-4 px-4">
-                  <div className="flex items-center">
-                    <button
-                      className="px-2 py-1 border-2 border-gray-300 text-gray-600 cursor-pointer hover:bg-emerald-900 hover:text-white"
-                      onClick={() =>
-                        handleDecrement(item.id)
-                      }
-                    >
-                      -
-                    </button>
-                    <input
-                      type="text"
-                      value={item.quantity}
-                      className="w-12 px-2 py-1 text-center border-t-2 border-b-2 border-gray-300"
-                      readOnly
-                    />
-                    <button
-                      className="px-2 py-1 border-2 border-gray-300 text-gray-600 cursor-pointer hover:bg-emerald-900 hover:text-white"
-                      onClick={() =>
-                        handleIncrement(item)
-                      }
-                    >
-                      +
-                    </button>
-                  </div>
-                </td>
-                <td className="py-4 px-4">
-                  ₹{(item.cost * item.quantity).toFixed(2)}
-                </td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan="6" className="py-4 px-4 text-center">
-                Your cart is empty
+                  <button
+                    className="px-2 py-1 border-2 border-gray-300 text-gray-600 cursor-pointer hover:bg-emerald-900 hover:text-white"
+                    onClick={() => handleIncrement(item)}
+                  >
+                    +
+                  </button>
+                </div>
+              </td>
+              <td className="py-4 px-4">
+                ₹{(item.cost * item.quantity).toFixed(2)}
               </td>
             </tr>
-          )}
+          ))}
         </tbody>
       </table>
-      <div className="mt-4 flex gap-8 mb-8">
-        <input
-          type="text"
-          placeholder="Coupon Code"
-          className="p-2 border border-gray-400"
-        />
-        <button className="bg-emerald-900 text-white px-4 py-1 cursor-pointer">
-          APPLY COUPON
-        </button>
-      </div>
     </div>
   );
 };
 
 const CartTotals = ({ orders }) => {
-  // Calculate the subtotal
-  const subtotal = orders
-    ? orders.reduce((sum, item) => sum + item.cost * item.quantity, 0)
-    : 0;
+  const Total = orders.reduce((sum, item) => sum + item.cost * item.quantity, 0);
+
+  const handlePayment = async () => {
+    try {
+      const { data } = await axios.post(
+        "http://localhost:3000/api/payments/initiate",
+
+        { amount: Total, currency: "INR" }, 
+        { headers: { Authorization: import.meta.env.VITE_APP_TOKEN } }
+      );
+
+      if (!data || !data.id) {
+        alert("Failed to create order");
+        return;
+      }
+
+      const options = {
+        key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+        amount: data.amount,
+        currency: "INR",
+        name: "Snacks E-com",
+        description: "Purchase Snacks",
+        order_id: data.id,
+        handler: async function (response) {
+          try {
+            const verifyRes = await axios.post(
+              "http://localhost:3000/api/payment/verify",
+              {
+                razorpay_order_id: response.razorpay_order_id,
+                razorpay_payment_id: response.razorpay_payment_id,
+                razorpay_signature: response.razorpay_signature,
+              },
+              { headers: { Authorization: import.meta.env.VITE_APP_TOKEN } }
+            );
+
+            if (verifyRes.data.success) {
+              alert("Payment successful!");
+            } else {
+              alert("Payment verification failed!");
+            }
+          } catch (error) {
+            console.error("Verification error:", error);
+            alert("Payment verification failed!");
+          }
+        },
+        prefill: {
+          name: "Ashish",
+          email: "ashish@example.com",
+          contact: "9876543210",
+        },
+        theme: { color: "#3399cc" },
+      };
+
+      const rzp = new window.Razorpay(options);
+      rzp.open();
+    } catch (error) {
+      console.error("Payment error:", error);
+      alert("Payment failed!");
+    }
+  };
 
   return (
     <div className="w-1/4 p-4 bg-white border-4 border-gray-200 rounded-lg shadow-lg ml-4 h-[40vh]">
       <h2 className="text-2xl font-bold mb-4">CART TOTALS</h2>
       <div className="flex justify-between mb-4 border-b border-gray-200 pb-4">
-        <span className="text-gray-700">Subtotal</span>
-        <span className="text-gray-900">₹{subtotal.toFixed(2)}</span>
-      </div>
-      {/* <div className="flex justify-between mb-4">
-        <span className="text-gray-700">Shipping</span>
-        <span className="text-gray-900">
-          Enter your address to view shipping options.
-        </span>
-      </div> */}
-      <div className="flex justify-between mb-4 border-b border-gray-200 pb-4">
-        <span className="text-gray-700">Shipping</span>
-        <span className="text-emerald-900 cursor-pointer">
-          Calculate shipping
-        </span>
-      </div>
-      <div className="flex justify-between mb-4 border-b border-gray-200 pb-4">
         <span className="text-gray-700">Total</span>
-        <span className="text-gray-900">₹{subtotal.toFixed(2)}</span>
+        <span className="text-gray-900">₹{Total.toFixed(2)}</span>
       </div>
-      <button className="w-full bg-emerald-900 text-white py-2 rounded-md hover:bg-green-700 transition duration-200">
+      <button
+        onClick={handlePayment}
+        className="w-full bg-emerald-900 text-white py-2 rounded-md hover:bg-green-700 transition duration-200"
+      >
         PROCEED TO CHECKOUT
       </button>
     </div>
@@ -193,23 +205,23 @@ const CartTotals = ({ orders }) => {
 };
 
 const Empty = () => {
-  return (
-    <div className="flex flex-col items-center justify-center w-[100vw] h-[70vh] bg-gray-100 mb-12">
-      <div className="text-gray-400 text-[25vh] mb-4">
-      <BsCart3 />
+    return (
+      <div className="flex flex-col items-center justify-center w-[100vw] h-[70vh] bg-gray-100 mb-12">
+        <div className="text-gray-400 text-[25vh] mb-4">
+        <BsCart3 />
+        </div>
+        <h2 className="text-5xl font-bold mb-2">Your cart is currently empty.</h2>
+        <p className="text-gray-600 m-4 text-center w-[35vw]">
+          Before proceeding to checkout, you must add some products to your shopping cart.
+          You will find a lot of interesting products on our "Shop" page.
+        </p>
+        <Link to="/shop">
+          <button className="bg-emerald-900 text-white px-6 py-2 rounded-md hover:bg-green-700 transition duration-200">
+            RETURN TO SHOP
+          </button>
+        </Link>
       </div>
-      <h2 className="text-5xl font-bold mb-2">Your cart is currently empty.</h2>
-      <p className="text-gray-600 m-4 text-center w-[35vw]">
-        Before proceeding to checkout, you must add some products to your shopping cart.
-        You will find a lot of interesting products on our "Shop" page.
-      </p>
-      <Link to="/shop">
-        <button className="bg-emerald-900 text-white px-6 py-2 rounded-md hover:bg-green-700 transition duration-200">
-          RETURN TO SHOP
-        </button>
-      </Link>
-    </div>
-  );
-};
+    );
+  };
 
 export default Cart;
